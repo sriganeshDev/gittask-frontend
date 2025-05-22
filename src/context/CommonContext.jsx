79a8bccdf721dd9f12521bfae5ff61f4fc5e4407
unknown <T079@CIPL-ATC00239.colanonline.local>
2025-05-22 11:04:27 +0530

    updated code

--- src/context/CommonContext.jsx
+++ src/context/CommonContext.jsx
@@ -1,33 +1,4 @@
-// // src/app/context/CommonContext.jsx
-// "use client";
 
-// import { createContext, useContext, useState } from "react";
-
-// const CommonContext = createContext();
-
-// export const ContextProvider = ({ children }) => {
-//   const token = localStorage.getItem("token");
-//   const [isLoggedIn, setIsLoggedIn] = useState(token);
-//   const [userData, setUserData] = useState([]);
-//   const [userReadMe, setUserReadMe] = useState([]);
-
-//   return (
-//     <CommonContext.Provider
-//       value={{
-//         isLoggedIn,
-//         setIsLoggedIn,
-//         userData,
-//         setUserData,
-//         userReadMe,
-//         setUserReadMe,
-//       }}
-//     >
-//       {children}
-//     </CommonContext.Provider>
-//   );
-// };
-
-// export const useCommonContext = () => useContext(CommonContext);
 
 "use client";
 
